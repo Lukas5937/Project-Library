fb9e2d1778cfe9695fdc4a03952be84e93984d7d
Lukas5937 <lukas-reich@t-online.de>
2023-11-10 13:38:13 +0700

    Add remove button to remove books from library

--- js-file.js
+++ js-file.js
@@ -1,5 +1,14 @@
-const  newBookButton = document.querySelector(".new-book-button");
+const openDialogButton = document.querySelector("#open-dialog-button");
 const dialog = document.querySelector("dialog");
+const addBookButton = document.querySelector("#add-book-button");
+const titleInput = document.querySelector("#title-input");
+const authorInput = document.querySelector("#author-input");
+const pagesInput = document.querySelector("#pages-input");
+const readInput = document.querySelector("#read-input");
+const read = document.querySelector('#read');
+const unread = document.querySelector('#unread');
+const dialogWrapper = document.querySelector(".dialog-wrapper");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -27,26 +36,53 @@ addBookToLibrary(theHobbit);
 addBookToLibrary(theBible);
 addBookToLibrary(thingsFallApart);
 
-myLibrary.forEach(function(book) {
-    bookGrid = document.querySelector(".book-grid")
+function createBookCard(book) {
     const bookCard = document.createElement("div");
     const titleText = document.createElement("p");
     const authorText = document.createElement("p");
     const pagesText = document.createElement("p");
     const readText = document.createElement("p");
-    bookCard.setAttribute("style", "padding: 16px; border: 1px solid grey; border-radius: 20px;")
+    const removeButton = document.createElement("button");
+    const bookGrid = document.querySelector(".book-grid");
+    bookCard.classList.add("book-card");
+    removeButton.textContent = "Remove";
+    removeButton.classList.toggle("remove-button");
     titleText.textContent = book.title;
     authorText.textContent = book.author;
     pagesText.textContent = book.pages;
     readText.textContent = book.read;
+    bookCard.setAttribute("data", myLibrary.indexOf(book));
     bookCard.appendChild(titleText);
     bookCard.appendChild(authorText);
     bookCard.appendChild(pagesText);
     bookCard.appendChild(readText);
+    bookCard.appendChild(removeButton);
     bookGrid.appendChild(bookCard);
+
+    removeButton.addEventListener("click", () => {
+        myLibrary.splice(bookCard.attributes.data.value, 1);
+        bookCard.setAttribute("data", myLibrary.indexOf(book));
+        while (bookGrid.firstChild) {
+            bookGrid.removeChild(bookGrid.lastChild);
+          }
+        myLibrary.forEach(createBookCard);
+    });
+};
+
+myLibrary.forEach(createBookCard);
+
+openDialogButton.addEventListener("click", () => {
+    dialog.showModal();
 });
 
-newBookButton.addEventListener("click", () => {
-    dialog.showModal();
-
-})
+addBookButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const read = readInput.value;
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+    createBookCard(newBook);
+    // dialogWrapper.removeChild(dialog);
+});
